dc80bcdaf92515709bb21e90ea91e1a2d82fc7a8
Jamil <jamil.jalal94@gmail.com>
2019-11-13 14:56:22 -0500

    Asset and login screen updated

---
+++ streetParker/streetParker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  streetParker
+//
+//  Created by Jamil Jalal on 11/13/19.
+//  Copyright © 2019 Jamil Jalal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var viewss: UIView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.viewss.layer.cornerRadius = 15.0
+    }
+
+
+}
+
---
+++ streetParker/streetParker/views/customizedButton.swift
@@ -0,0 +1,18 @@
+//
+//  customizedButton.swift
+//  streetParker
+//
+//  Created by Jamil Jalal on 11/13/19.
+//  Copyright © 2019 Jamil Jalal. All rights reserved.
+//
+
+import UIKit
+
+class customizedButton: UIButton {
+
+  
+    override func awakeFromNib() {
+        layer.cornerRadius = 10
+        layer.backgroundColor = #colorLiteral(red: 0.2642766497, green: 0.2642766497, blue: 0.2642766497, alpha: 0.5288152825)
+    }
+}
