adfaba749ba1f44926d7942896a4c4c0d1a5b18c
Jamil <jamil.jalal94@gmail.com>
2019-11-18 01:15:25 -0500

    files added for sw reveal

---
+++ streetParker/streetParker/Controllers/burgerMenuVC.swift
@@ -0,0 +1,30 @@
+//
+//  burgerMenuVC.swift
+//  streetParker
+//
+//  Created by Jamil Jalal on 11/18/19.
+//  Copyright © 2019 Jamil Jalal. All rights reserved.
+//
+
+import UIKit
+
+class burgerMenuVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
